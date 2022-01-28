fd9b71205feabf209dca4b975df487cf226b99d6
SushantKhanal <dreamerking114@gmail.com>
2022-01-28 15:35:53 -0600

    test /api/members api with postman

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 1,
+        name: 'Samuel Doe',
+        email: 'sam@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 1,
+        name: 'Debora Doe',
+        email: 'deb@gmail.com',
+        status: 'active'
+    },
+];
+
+module.exports = members;
