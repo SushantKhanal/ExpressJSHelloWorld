38f6c23b71b1ee1252b5220ea1ce6f043e27448c
SushantKhanal <dreamerking114@gmail.com>
2022-01-28 17:14:53 -0600

    form is working

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const path = require('path');
-const logger = require('./middleware/logger')
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
-//Init middleware
-//app.use(logger);
+// Init middleware
+// app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-//Set static folder
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App!',
+    members
+}));
+
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-app.use('/api/members', require('./routes/api/members'))
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
